e3e5480790810eb89814c3051b636c3352609806
Vytis Vadoklis <vytis.vadoklis@outlook.com>
2018-03-13 12:31:35 +0000

    finalized both files

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,2 +1,29 @@
-def fizzbuzz
+def fizzbuzz(number)
+  case
+  when number % 15 == 0 then "fizzbuzz"
+  when number % 3  == 0 then "fizz"
+  when number % 5  == 0 then "buzz"
+  else number
+  end
 end
+
+puts fizzbuzz(1)			
+puts fizzbuzz(2)
+puts fizzbuzz(3)
+puts fizzbuzz(4)
+puts fizzbuzz(5)
+puts fizzbuzz(6)
+puts fizzbuzz(7)
+puts fizzbuzz(8)
+puts fizzbuzz(9)
+puts fizzbuzz(10)
+puts fizzbuzz(11)
+puts fizzbuzz(12)
+puts fizzbuzz(13)
+puts fizzbuzz(14)
+puts fizzbuzz(15)
+puts fizzbuzz(16)
+puts fizzbuzz(17)
+puts fizzbuzz(18)
+puts fizzbuzz(19)
+puts fizzbuzz(20)
